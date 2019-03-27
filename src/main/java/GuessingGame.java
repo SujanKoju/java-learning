441a2ffa6665b8e4b9598a9b983079f0788654e4
SUJAN KOJU <sujan.koju@citytech.global>
2019-03-27 14:23:22 +0545

    GuessingGame Added

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public void startGuessingGame()
+    {
+        int randomNo = (int) (Math.random() * 100) + 1;
+        boolean hasWon = false;
+
+        System.out.println("I have randomly chosen no between 1-100");
+        System.out.println("********* TRY TO GUESS **********");
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int noOfGuessLeft = 10; noOfGuessLeft > 0; noOfGuessLeft--) {
+            System.out.println("You have " + noOfGuessLeft + " Guess Left.");
+            int guess = scanner.nextInt();
+            if (randomNo < guess) {
+                System.out.println("It is Less than " + guess);
+            } else if (randomNo > guess) {
+                System.out.println("It is Greater than " + guess);
+            } else {
+                hasWon = true;
+                break;
+            }
+        }
+        if (hasWon) {
+            System.out.println("Correct Guess ..... YOU WIN .......");
+        } else {
+            System.out.println("GAME OVER.....YOU LOSE.....");
+            System.out.println("The No was "+randomNo);
+        }
+    }
+
+    public static void main(String []args) {
+        GuessingGame game = new GuessingGame();
+        game.startGuessingGame();
+
+    }
+}
